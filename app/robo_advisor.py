9fea3a0c998ff87addc7f070b223361dbb5d0de2
ts2905 <84340990+ts2905@users.noreply.github.com>
2021-06-13 19:13:53 -0400

    Fetch data from the API

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -16,3 +16,18 @@ print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv() 
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="demo")
+
+symbol = "MFST"
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+response = requests.get(request_url)
+print(type(response))
+
+print(response.text)
